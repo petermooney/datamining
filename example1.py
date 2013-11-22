86d222920357494e15222565504659a247eb304d
Dr. Peter Mooney <peter.mooney@nuim.ie>
2013-11-22 12:01:05 +0000

    Added information about the purpose of the code

--- example1.py
+++ example1.py
@@ -1,4 +1,19 @@
+### This is source code used for an invited lecture on Data Mining using Python for 
+### the Institute of Technology at Blanchardstown, Dublin, Ireland
+### Lecturer and presenter: Dr. Peter Mooney
+### email: peter.mooney@nuim.ie
+### Date: November 2013
+###
+### The purpose of this lectures is to provide students with an easily accessible overview, with working 
+### examples of how Python can be used as a tool for data mining. 
+### For those using these notes and sample code: This code is provided as a means of showing some basic ideas around 
+### data extraction, data manipulation, and data visualisation with Python. 
+### The code provided could be written in many different ways as is the Python way. However I have tried to keep things simple and practical so that students can get an understanding of the process of data mining rather than this being a programming course in Python. 
+###
+### If you use this code - please give me a little citation with a link back to the GitHub Repo where you found this piece of code: https://github.com/petermooney/datamining
+
 import csv
+
 
 def readCSVFile(filename):
 
@@ -9,6 +24,7 @@ def readCSVFile(filename):
 			
 			
 def main():
+	# we will read in the CSV file with the list of Unesco Heritage Sites world wide. 
 	readCSVFile("unesco.csv")
 	
 	
