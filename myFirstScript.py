dce8c0abda4e2816e5505937ec8cede16cf7e02a
Dr. Peter Mooney <peter.mooney@nuim.ie>
2013-12-04 08:08:19 +0000

    A very simple introductory script in Python. This will be the first script you will run

---
+++ myFirstScript.py
@@ -0,0 +1,14 @@
+
+
+def main():
+
+	simpleMethod(103)
+
+def simpleMethod(inputNumber):
+
+	if inputNumber >= 100:
+		print ("The number {} is >= 100".format(inputNumber))
+	else:
+		print ("The number {} is < 100".format(inputNumber))		
+
+main()
